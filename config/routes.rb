983e545ae5c8f51af68eaeacb9580cfe1adc522d
Mickael Tremauville <71016683+mtremauville@users.noreply.github.com>
2026-09-08 19:12:35 +0200

    feat: ajoute le CRUD produit (index, show)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "products/index"
+  get "products/show"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,5 +14,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: "home#index"
+  resources :products, only: [:index, :show]
+  root "products#index"
 end
